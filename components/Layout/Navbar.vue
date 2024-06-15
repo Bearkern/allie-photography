d66716aff1a11e14498f63093395bc74c3e39fb5
Bearkern <haolin1224@gmail.com>
2024-06-15 14:17:37 +0800

    feat: add desktop Navbar

--- components/Layout/Navbar.vue
+++ components/Layout/Navbar.vue
@@ -4,7 +4,6 @@ const isHamburgerMenuVisible = ref(false);
 
 const toggleHamburgerMenu = () => {
   isHamburgerMenuVisible.value = !isHamburgerMenuVisible.value;
-  console.log("isHamburgerMenuVisible.value:", isHamburgerMenuVisible.value);
 };
 
 watch(
@@ -25,21 +24,31 @@ watch(
         ></NuxtLink>
         艾里攝影工作室
       </h1>
-      <button @click="toggleHamburgerMenu">
-        <img src="@/assets/images/bars.svg" alt="漢堡選單" />
-      </button>
-      <nav
-        class="absolute bg-secondary text-white w-full max-h-0 top-full left-0 z-10 transition-max-height duration-300"
-        :class="{ 'max-h-96': isHamburgerMenuVisible }"
-      >
-        <ul class="flex flex-col gap-4 text-center py-4">
-          <li><NuxtLink to="/about">關於艾里</NuxtLink></li>
-          <li><NuxtLink to="/portfolios">攝影作品</NuxtLink></li>
-          <li><NuxtLink to="/packages">攝影方案</NuxtLink></li>
-          <li><NuxtLink to="/contact">聯絡我們</NuxtLink></li>
-          <li><NuxtLink to="/booking">預約攝影</NuxtLink></li>
-        </ul>
-      </nav>
+      <div>
+        <button class="md:hidden" @click="toggleHamburgerMenu">
+          <span class="material-symbols-outlined text-[40px]"> menu </span>
+        </button>
+        <nav
+          class="absolute bg-secondary text-white w-full max-h-0 top-full left-0 z-10 transition-max-height duration-300 md:static md:max-h-auto md:bg-white md:text-secondary md:transition-none"
+          :class="{ 'max-h-96': isHamburgerMenuVisible }"
+        >
+          <ul
+            class="flex flex-col gap-4 text-center py-4 md:flex-row lg:gap-10"
+          >
+            <li><NuxtLink to="/about">關於艾里</NuxtLink></li>
+            <li><NuxtLink to="/portfolios">攝影作品</NuxtLink></li>
+            <li><NuxtLink to="/packages">攝影方案</NuxtLink></li>
+            <li><NuxtLink to="/contact">聯絡我們</NuxtLink></li>
+            <li>
+              <NuxtLink to="/booking"
+                ><span class="material-symbols-outlined">
+                  shopping_cart
+                </span></NuxtLink
+              >
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 </template>
