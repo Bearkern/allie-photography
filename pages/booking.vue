28c5e7d98bc7fef6b0cad8de909ee0b0e7ad45b5
Bearkern <haolin1224@gmail.com>
2024-10-01 17:26:33 +0800

    feat: Added booing banner and noticeOC

--- pages/booking.vue
+++ pages/booking.vue
@@ -1,9 +1,22 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <h1>線上預約</h1>
-  </div>
+  <section class="min-h-[300px] bg-gradient-to-l from-primary to-light bg-cover bg-center">
+    <div class="absolute inset-0 bg-primary opacity-20"></div>
+    <div class="container relative z-10 flex h-[300px] items-center">
+      <h2>線上預約</h2>
+    </div>
+  </section>
+
+  <section class="container py-10 lg:py-16">
+    <h3 class="mb-1">預訂注意事項</h3>
+    <ul>
+      <li>如需假日拍攝需在懷孕週數20週以前預約拍攝檔期(建議拍攝週數為孕期28-32週)</li>
+      <li>拍攝成品皆同意無償供工作室刊登、使用（如不能公開請於簽約時告知）</li>
+      <li>選定方案簽約後恕無更改方案</li>
+      <li>◎本公司工作人員均已完成新冠肺炎疫苗完全接種◎</li>
+    </ul>
+  </section>
 </template>
 
 <style scoped></style>
