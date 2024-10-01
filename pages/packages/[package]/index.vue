811fbc8a75f7d9f5ff72159a754cc9408b512340
Bearkern <haolin1224@gmail.com>
2024-10-01 16:28:13 +0800

    feat: Render package detail information

--- pages/packages/[package]/index.vue
+++ pages/packages/[package]/index.vue
@@ -1,10 +1,16 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+import { usePackageStore } from '@/stores/package';
+
+const packageStore = usePackageStore();
+const { photoPackage } = storeToRefs(packageStore);
+</script>
 
 <template>
   <div>
     <h3 class="mb-1">方案詳細介紹</h3>
     <ul>
-      <!-- <li v-for="(item, index) in photoPackages.detail" :key="index + 1">{{ item }}</li> -->
+      <li v-for="(item, index) in photoPackage.detail" :key="index + 1">{{ item }}</li>
     </ul>
   </div>
 </template>
--- pages/packages/[package]/notice.vue
+++ pages/packages/[package]/notice.vue
@@ -1,9 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+import { usePackageStore } from '@/stores/package';
+
+const packageStore = usePackageStore();
+const { photoPackage } = storeToRefs(packageStore);
+</script>
 
 <template>
   <div>
     <h3 class="mb-1">預訂注意事項</h3>
-    <!-- <div v-html="photoPackages.notice"></div> -->
+    <div v-html="photoPackage.notice"></div>
   </div>
 </template>
 
